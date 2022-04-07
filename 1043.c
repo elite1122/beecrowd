c5a49994c89466c0b483c01e6078d029018f7ad4
Md. Rasheduzzaman Elite <elitetuber1122@gmail.com>
2022-04-07 10:49:53 +0600

    Triangle
    https://www.beecrowd.com.br/judge/en/problems/view/1043

---
+++ 1043.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	float A,B,C;
+	scanf("%f %f %f",&A,&B,&C);
+	if(A+B>C && B+C>A && C+A>B)
+	{
+		printf("Perimetro = %.1lf\n",A+B+C);
+	}
+	else
+	{
+		printf("Area = %.1lf\n",((A+B)*C)/2);
+	}
+	return 0;
+}
